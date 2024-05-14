38ccf47359358bd75742c06914ed87c7cf9fd5dc
jaehafe <wogkdkrm112@gmail.com>
2024-05-14 20:33:15 +0900

    feat: join process

---
+++ src/main/java/com/oop/security/dto/JoinDTO.java
@@ -0,0 +1,12 @@
+package com.oop.security.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class JoinDTO {
+
+    private String username;
+    private String password;
+}
