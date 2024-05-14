3ac1661222b52426d9d814abb34e7e10e93d718a
jaehafe <wogkdkrm112@gmail.com>
2024-05-14 20:08:44 +0900

    feat: UserEntity, UserRepository

---
+++ src/main/java/com/oop/security/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.oop.security.repository;
+
+import com.oop.security.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+
+}
