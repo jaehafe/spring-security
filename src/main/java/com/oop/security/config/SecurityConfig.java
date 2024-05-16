3d9b4d61e7f41a09870de88d3c4006bbd253ad74
jaehafe <wogkdkrm112@gmail.com>
2024-05-16 19:59:41 +0900

    feat: refresh로 access 재발급

--- src/main/java/com/oop/security/config/SecurityConfig.java
+++ src/main/java/com/oop/security/config/SecurityConfig.java
@@ -90,6 +90,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/reissue").permitAll()
                         .anyRequest().authenticated()
                 );
 
